9fca377e50d05cbb918f408bd043267f8f253d1b
tatsuya.orikasa <peter-porker.ham@yahoo.ne.jp>
2020-11-21 14:21:22 +0000

    tasklist add user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,13 @@
 class TasksController < ApplicationController
     before_action :set_task, only: [:show, :edit, :update, :destroy]
+    before_action :require_user_logged_in
+    before_action :correct_user, only: [:destroy]
     
     def index
-      @tasks = Task.all.page(params[:page]).per(5)
+      if logged_in?
+        @tasks = current_user.tasks.build
+        @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+      end
     end
 
     def show
@@ -13,7 +18,7 @@ class TasksController < ApplicationController
     end
     
     def create
-      @task = Task.new(task_params)
+      @task = current_user.tasks.build(task_params)
     
       if @task.save
         flash[:success] = 'Task の追加が完了しました'
@@ -53,5 +58,12 @@ class TasksController < ApplicationController
     
     def task_params
         params.require(:task).permit(:content,:status)
+    end
+    
+    def correct_user
+      @task = current_user.tasks.find_by(id: params[:id])
+      unless @task
+        redirect_to root_url
+      end
     end
 end
